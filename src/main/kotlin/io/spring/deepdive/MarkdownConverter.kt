9ae0a78de5180541c62a6e14bc8386028266337a
frederic.montariol <frederic.montariol@adventiel.fr>
2017-12-18 12:40:45 +0100

    chore : use Kotlin's listOf instead of JDK's Arrays.asList

--- src/main/kotlin/io/spring/deepdive/MarkdownConverter.kt
+++ src/main/kotlin/io/spring/deepdive/MarkdownConverter.kt
@@ -15,18 +15,15 @@
  */
 package io.spring.deepdive
 
-import java.util.Arrays
-
 import org.commonmark.ext.autolink.AutolinkExtension
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
-
 import org.springframework.stereotype.Service
 
 @Service
 class MarkdownConverter : (String?) -> String {
 
-    private val parser = Parser.builder().extensions(Arrays.asList(AutolinkExtension.create())).build()
+    private val parser = Parser.builder().extensions(listOf(AutolinkExtension.create())).build()
     private val renderer = HtmlRenderer.builder().build()
 
     override fun invoke(input: String?): String {
