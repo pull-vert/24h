c2ec8f66aa70979dc4fe9e517c3a6657f525e4f6
frederic.montariol <frederic.montariol@adventiel.fr>
2017-12-20 15:08:44 +0100

    migrate to java 9

---
+++ src/main/kotlin/module-info.java
@@ -0,0 +1,18 @@
+module one.at.a.time {
+    requires kotlin.stdlib;
+    requires spring.boot.autoconfigure;
+    requires spring.data.mongodb;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.security.core;
+    requires reactor.core;
+    requires commonmark;
+    requires commonmark.ext.autolink;
+    requires spring.webflux;
+    requires spring.web;
+    requires spring.security.config;
+    requires spring.data.commons;
+    requires java.validation;
+
+    exports one.at.a.time;
+}
