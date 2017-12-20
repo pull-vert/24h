905b81bc4b3c009877a40b433af3c252de910513
frederic.montariol <frederic.montariol@adventiel.fr>
2017-12-20 15:06:26 +0100

    change base package

---
+++ src/main/kotlin/one/at/a/time/model/Auditable.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2002-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.at.a.time.model
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.domain.Persistable
+import java.time.LocalDateTime
+import java.util.*
+
+abstract class Auditable(
+        @Id private val id: Any = UUID.randomUUID(),
+        @CreatedDate var createdDate: LocalDateTime? = null,
+        @LastModifiedDate var lastModifiedDate: LocalDateTime? = null): Persistable<Any> {
+    // Persistable functions
+    override fun isNew() = (null == createdDate)
+    override fun getId() = id
+}
---
+++ src/test/kotlin/one/at/a/time/web/withSecurity.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2002-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.at.a.time.web
+
+import org.springframework.web.reactive.function.client.WebClient
+import java.util.*
+
+private fun buildAutorizationHeader(): String {
+    val auth = "sdeleuze:password"
+    val encodedAuth = Base64.getEncoder().encode(
+            auth.toByteArray(Charsets.UTF_8))
+    return "Basic " + String(encodedAuth)
+}
+
+fun WebClient.RequestHeadersSpec<*>.addAuthHeader() =
+        this.header("Authorization", buildAutorizationHeader())
