dfee6e615ed9dc318816a584c754005639dccbad
pull-vert <frederic.montariol@gmail.com>
2019-02-03 16:20:38 +0100

    simplify Application

--- src/main/kotlin/one/at/a/time/Application.kt
+++ src/main/kotlin/one/at/a/time/Application.kt
@@ -17,12 +17,10 @@ package one.at.a.time
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.mongodb.config.EnableMongoAuditing
 
 @SpringBootApplication
-@EnableMongoAuditing
 class Application
 
-fun main(args: Array<String>) {
-    runApplication<Application>(*args)
+fun main() {
+    runApplication<Application>()
 }
