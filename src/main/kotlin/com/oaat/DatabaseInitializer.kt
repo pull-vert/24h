971ce2c48f8ef5c36ffceb20e25303965885364b
pull-vert <frederic.montariol@gmail.com>
2019-02-06 17:49:32 +0100

    update gradle to 5.2 and kotlin to 1.3.21. Support java 9 module

--- src/main/kotlin/com/oaat/DatabaseInitializer.kt
+++ src/main/kotlin/com/oaat/DatabaseInitializer.kt
@@ -17,89 +17,75 @@ package com.oaat
 
 import com.oaat.entities.Post
 import com.oaat.entities.PostEvent
-import one.at.a.time.model.Role.ADMIN
-import one.at.a.time.model.Role.USER
-import one.at.a.time.model.User
+import com.oaat.entities.User
 import com.oaat.repositories.PostRepository
-import one.at.a.time.repository.UserRepository
+import com.oaat.services.UserService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.data.mongodb.core.CollectionOptions
 import org.springframework.data.mongodb.core.MongoOperations
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import reactor.core.publisher.toFlux
-import java.time.LocalDateTime
+import java.util.*
 
+internal const val USER_FRED_UUID = "79e9eb45-2835-49c8-ad3b-c951b591bc7f"
+internal const val USER_BOSS_UUID = "67d4306e-d99d-4e54-8b1d-5b1e92691a4e"
 
 @Component
 class DatabaseInitializer(
         private val ops: MongoOperations,
-        private val userRepository: UserRepository,
-        private val postRepository: PostRepository,
-        private val passwordEncoder: PasswordEncoder) : CommandLineRunner {
+        private val userService: UserService,
+        private val postRepository: PostRepository) : CommandLineRunner {
 
     override fun run(vararg args: String) {
         println("DatabaseInitializer start")
-        ops.dropCollection(PostEvent::class.java)
+
+        // uncomment if targetting a real MongoDB Database (not embedded)
+//        ops.dropCollection(PostEvent::class.java)
+//        userService.deleteAll()
+//                .block()
+//        postRepository.deleteAll()
+//                .block()
+
         ops.createCollection(PostEvent::class.java, CollectionOptions.empty().capped().size(10000))
 
-        val min = User("min",  passwordEncoder.encode("password"))
-        val seb = User("sdeleuze", passwordEncoder.encode("password"), mutableListOf(USER, ADMIN))
-        val simon = User("simonbasle", passwordEncoder.encode("password"))
-
-        userRepository.deleteAll()
-                .block()
-
-        listOf(min, seb, simon)
+        val fred = User("Fred", "password", id = UUID.fromString(USER_FRED_UUID), enabled = true)
+        val boss = User("Boss", "secured_password", id = UUID.fromString(USER_BOSS_UUID), enabled = true)
+        listOf(fred, boss)
                 .toFlux()
-                .flatMap {
-                    println("saving ${it.username}")
-                    userRepository.save(it)
+                .flatMap { user -> userService.save(user) }
+                .doOnNext { user ->
+                    println("saving user $user, entity informations; createdBy=${user.createdBy}, createdDate=${user.createdDate}")
                 }.blockLast()
+
 
         val reactorTitle = "Reactor Bismuth is out"
         val reactorPost = Post(
-                reactorTitle.slugify(),
+//                reactorTitle.slugify(),
                 reactorTitle,
                 """It is my great pleasure to announce the GA release of **Reactor Bismuth**, which notably encompasses
                     |`reactor-core` **3.1.0.RELEASE** and `reactor-netty` **0.7.0.RELEASE** \uD83C\uDF89""".trimMargin(),
-                """With the release of [Spring Framework 5.0](https://spring.io/blog/2017/09/28/spring-framework-5-0-goes-ga)
-                    |now just happening, you can imagine this is a giant step for Project Reactor :)""".trimMargin(),
-                "simonbasle",
-                LocalDateTime.of(2017, 9, 28, 12, 0)
+                "simonbasle"
         )
 
         val springTitle = "Spring Framework 5.0 goes GA"
         val spring5Post = Post(
-                springTitle.slugify(),
                 springTitle,
                 """Dear Spring community,
                     |It is my pleasure to announce that, after more than a year of milestones and RCs and almost two years of development overall,
                     |Spring Framework 5.0 is finally generally available as 5.0.0.RELEASE from [repo.spring.io](https://repo.spring.io)
                     |and Maven Central!""".trimMargin(),
-                """This brand-new generation of the framework is ready for 2018 and beyond: with support for JDK 9 and the Java EE 8 API level (e.g. Servlet 4.0),
-                    |as well as comprehensive integration with Reactor 3.1, JUnit 5, and the Kotlin language. On top of that all, Spring Framework 5 comes with
-                    |many functional API variants and introduces a dedicated reactive web framework called Spring WebFlux, next to a revised version of our
-                    |Servlet-based web framework Spring MVC.""".trimMargin(),
-                "springjuergen",
-                LocalDateTime.of(2017, 9, 28, 11, 30)
+                "springjuergen"
         )
 
         val postTitle = "Introducing Kotlin support in Spring Framework 5.0"
         val springKotlinPost = Post(
-                postTitle.slugify(),
                 postTitle,
-                """Following the [Kotlin support on start.spring.io](https://spring.io/blog/2016/02/15/developing-spring-boot-applications-with-kotlin)
-                    |we introduced a few months ago, we have continued to work to ensure that Spring and [Kotlin](https://kotlin.link/) play well together.""".trimMargin(),
                 """One of the key strengths of Kotlin is that it provides a very good [interoperability](https://kotlinlang.org/docs/reference/java-interop.html)
                     |with libraries written in Java. But there are ways to go even further and allow writing fully idiomatic Kotlin code when developing your next
                     |Spring application. In addition to Spring Framework support for Java 8 that Kotlin applications can leverage like functional web or bean registration APIs,
                     |there are additional Kotlin dedicated features that should allow you to reach a new level of productivity.""".trimMargin(),
-                "sdeleuze",
-                LocalDateTime.of(2017, 1, 4, 9, 0)
+                "sdeleuze"
         )
-        postRepository.deleteAll()
-                .block()
 
         listOf(reactorPost, spring5Post, springKotlinPost)
                 .toFlux()
