32033f9855f4a06550026dfcaa8d996982e4a672
pull-vert <frederic.montariol@gmail.com>
2019-04-08 15:49:52 +0200

    simplify my name :)

--- src/main/kotlin/com/oaat/web/handlers/UserHandler.kt
+++ src/main/kotlin/com/oaat/web/handlers/UserHandler.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 OAAT's author : Frédéric Montariol. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2018-2019 OAAT's author : Fred Montariol. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package com.oaat.web.handlers
